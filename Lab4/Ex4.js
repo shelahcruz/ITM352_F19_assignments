da4a7aaad4a97b24d33ad29e970de874c51591dc
shelahcruz <54688786+shelahcruz@users.noreply.github.com>
2019-10-02 16:51:25 -1000

    Create Ex4.js

---
+++ Lab4/Ex4.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+    console.log(req.headers); //output the request headers to the console
+    res.writeHead(200, { 'Content-Type': 'text/html' }); // set MIME type to HTML 
+   res.write('<window.location = "http://www.apple.com";</script>');
+    res.write('<META http- equiv ="refresh" content="0;URL=http://www.google.com">'); //write a response to the client
+    //res.write('<script>window.location = "http://www.apple.com";</script>') 
+    //res.writeHead(301, { "Location": "http://amazon.com"}); 
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+console.log('Hello world HTTP server listening on localhost port 8080');
