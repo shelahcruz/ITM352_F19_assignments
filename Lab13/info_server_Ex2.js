f2ccd4e2b80300932a49ec737ce4eb70ce708635
shelahcruz <54688786+shelahcruz@users.noreply.github.com>
2019-11-13 12:06:24 -1000

    Update info_server_Ex2.js

--- Lab13/info_server_Ex2.js
+++ Lab13/info_server_Ex2.js
@@ -5,14 +5,32 @@ var app = express();
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
-
 });
 
 app.use(myParser.urlencoded({ extended: true }));
 app.post("/process_form", function (request, response) {
    let POST = request.body;
-   response.send(POST); 
+   if (typeof POST['quantity_textbox'] != 'undefined') {
+    displayPurchase(POST, response);
+} 
 });
 
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
+
+function isNonNegInt(q, return_errors = false) {
+    errors = []; // assume no errors at first
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return return_errors ? errors : (errors.length == 0);
+}
+
+function displayPurchase(POST, response) {
+    q = POST['quantity_textbox'];
+    if (isNonNegInt(q)) {
+        response.send(`Thank you for purchasing ${q} things!`);  
+    } else {
+        response.send(`${q} is not a quantity! Press the back button and try again.`); 
+    }
+}
