9c4aa554f03fe32b124928ac7e29c2414b91a65d
shelahcruz <54688786+shelahcruz@users.noreply.github.com>
2019-11-14 19:44:05 -1000

    Create product_data.js

---
+++ Cruz_ShelahMarie_Assignment1/public/product_data.js
@@ -0,0 +1,49 @@
+var products =
+
+[
+// Product 1
+    {
+    "brand": "Vans Checkboard",
+    "price": 55.00,
+    "image": './images/Vans-SlipOn.jpg'
+    },
+            
+// Product 2
+    {
+    "brand": "Vans Old Skool",
+    "price": 60.00,
+    "image": './images/Vans-OldSkool.jpg'
+    },
+
+// Product 3
+    {
+    "brand": "Converse Chuck Taylor",
+    "price": 65.00,
+    "image": './images/Converse.jpg'
+    },
+
+// Product 4
+    {
+    "brand": "Nike Air Force 1",
+    "price": 100.00,
+    "image": './images/Nike-AirForce1.jpg'
+    },
+
+// Product 5
+    {
+    "brand": "Air Jordan Retro 12",
+    "price": 140.00,
+    "image": './images/AirJordan.jpg'
+    },
+
+// Product 6
+    {
+    "brand": "Yeezy Boost 350",
+    "price": 495.00,
+    "image": './images/Yeezy.jpg'
+    },
+];
+
+if (typeof module != 'undefined') {
+    module.exports.products = products;
+    }
