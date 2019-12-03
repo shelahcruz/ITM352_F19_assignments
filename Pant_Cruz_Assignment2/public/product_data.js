47ba40ed669b115bef62735be04b4d2f4fa7ac06
shelahcruz <54688786+shelahcruz@users.noreply.github.com>
2019-12-02 21:56:46 -1000

    Create product_data.js

---
+++ Pant_Cruz_Assignment2/public/product_data.js
@@ -0,0 +1,34 @@
+/*PREETHA PANT, 11/26/19*/
+/* products array, listing all my products prices and corresponding images*/
+products = [
+    {
+        "product": "Towels",
+        "price": 10.00,
+        "image": "towels.jpg"
+    },
+    {
+        "product": "Swimsuit",
+        "price": 30.00,
+        "image": "swimsuit.jpg"
+    },
+    {
+        "product": "Sunscreen",
+        "price": 7.00,
+        "image": "sunscreen.jpg"
+    },
+    {
+        "product": "Hat",
+        "price": 12.00,
+        "image": "hat.jpg"
+    },
+    {
+        "product": "Sunglasses",
+        "price": 45.00,
+        "image": "sunglasses.jpg"
+    }
+];
+
+if (typeof module != 'undefined') {
+    module.exports = products;//get the product 
+}
+
